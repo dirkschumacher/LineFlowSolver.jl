00f7609fbb095788e603ab84e489b931966b3fef
Dirk Schumacher <mail@dirk-schumacher.net>
2017-06-03 11:38:45 +0200

    Organize tests and minor improvements to code

--- src/solver.jl
+++ src/solver.jl
@@ -2,26 +2,26 @@ function solve(solver::MathProgBase.SolverInterface.AbstractMathProgSolver,
                 problem::LineFlowProblem)
 
     # Lines
-    L = problem.lines
+    const L = problem.lines
 
     # Nodes
-    N = problem.nodes
+    const N = problem.nodes
 
     # Edges
-    E = problem.edges
+    const E = problem.edges
 
     # fixed edges
-    FE = problem.fixed_edges
+    const FE = problem.fixed_edges
 
     # single flow edges
-    SE = problem.single_flow_edges
+    const SE = problem.single_flow_edges
 
     # edge cost
-    cost = problem.cost
+    const cost = problem.cost
 
     # check if (edge, line) tuple is valid
     function is_valid_var(e, l)
-        fe_edges = filter(x -> x[1] == e, FE)
+        const fe_edges = filter(x -> x[1] == e, FE)
         isempty(fe_edges) || length(filter(x -> x[2] == l, fe_edges)) == 1
     end
 
@@ -32,7 +32,7 @@ function solve(solver::MathProgBase.SolverInterface.AbstractMathProgSolver,
     @variable(model, 0 <= x[e = 1:length(E), l = L; is_valid_var(e, l)] <= 1, Bin)
 
     # Minimize the edge cost for all assigned edge/line tuples
-    valid_el = filter(x -> is_valid_var(x[1], x[2]), [(i,l) for i = 1:length(E) for l = L])
+    const valid_el = filter(x -> is_valid_var(x[1], x[2]), [(i,l) for i = 1:length(E) for l = L])
     @objective(model, Min, sum(cost[e[1]] * x[e[1],e[2]] for e = valid_el))
 
     #Fix edges
@@ -41,9 +41,9 @@ function solve(solver::MathProgBase.SolverInterface.AbstractMathProgSolver,
     end
 
     # fix single edges
-    fixed_edge_indexes = map(x -> x[1], FE)
+    const fixed_edge_indexes = map(x -> x[1], FE)
     for e in SE
-        ls = filter(l -> is_valid_var(e, l), L)
+        const ls = filter(l -> is_valid_var(e, l), L)
         if e ∉ fixed_edge_indexes
             @constraint(model, sum(x[e,l] for l =ls) <= 1)
         end
@@ -51,11 +51,11 @@ function solve(solver::MathProgBase.SolverInterface.AbstractMathProgSolver,
 
     # Flow constraint
     for v in N
-        inbound_edges = map(x -> first(indexin([x], E)), filter(x -> x[2] == v, E))
-        outbound_edges = map(x -> first(indexin([x], E)), filter(x -> x[1] == v, E))
+        const inbound_edges = map(x -> first(indexin([x], E)), filter(x -> x[2] == v, E))
+        const outbound_edges = map(x -> first(indexin([x], E)), filter(x -> x[1] == v, E))
         for l in L
-            v_inbound_edges = filter(x -> is_valid_var(x[1], l), inbound_edges)
-            v_outbound_edges = filter(x -> is_valid_var(x[1], l), outbound_edges)
+            const v_inbound_edges = filter(x -> is_valid_var(x[1], l), inbound_edges)
+            const v_outbound_edges = filter(x -> is_valid_var(x[1], l), outbound_edges)
             if !isempty(v_inbound_edges) && !isempty(v_outbound_edges)
                 @constraint(model, sum(x[e,l] for e = v_inbound_edges) ==
                                 sum(x[e,l] for e = v_outbound_edges))
@@ -66,7 +66,7 @@ function solve(solver::MathProgBase.SolverInterface.AbstractMathProgSolver,
     # Solve it
     JuMP.solve(model)
 
-    x_val = getvalue(x)
+    const x_val = getvalue(x)
     assignment = []
     for e in 1:length(E)
         for l in L
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,33 +2,14 @@ using LineFlowSolver
 using Base.Test
 using JuMP
 using GLPKMathProgInterface
+using LightGraphs
 
-# lines
-lines = [1, 2]
+tests = ["solve"]
 
-# Nodes
-nodes = [1, 2, 3, 4, 5, 6, 7, 8]
+println("Running tests:")
 
-# Edges
-edges = [(1, 3), (2, 4),
-     (3, 4), (4, 3),
-     (3, 5), (4, 6),
-     (5, 6), (6, 5),
-     (5, 7), (6, 8)]
-
-# fixed edges
-FE = [(1, 1), (2, 2), (9, 1), (10, 2)]
-
-# single flow edges
-SE = [5, 6]
-
-# edge cost
-cost = [0, 0, 1, 1, 0, 0, 1, 1, 0, 0]
-
-problem = LineFlowProblem(nodes, edges, lines, FE, SE, cost)
-solution = LineFlowSolver.solve(GLPKSolverMIP(msg_lev = 3), problem)
-
-@test FE ⊆ solution.assignment
-@test (5, 1) in solution.assignment
-@test (6, 2) in solution.assignment
-@test 0 == solution.objective_value
+for t in tests
+    test_fn = "$t.jl"
+    println(" * $test_fn")
+    include(test_fn)
+end
